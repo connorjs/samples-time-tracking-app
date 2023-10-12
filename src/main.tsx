a05157963682b03b4547788af2003c3b348140be
Connor Sullivan <dev.connorjs@gmail.com>
2023-10-12 14:21:05 -0400

    🚨 Use eslint-config-connorjs
    Installs `eslint-config-connorjs` and fixes lint errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import {App} from './app.tsx'
+
+/* globals document -- web browser */
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- index.html
+ReactDOM.createRoot(document.querySelector(`#root`)!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
